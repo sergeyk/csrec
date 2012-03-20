4dde22a706dcbb1afd3b72cc5d49cb65b638b5eb
Ron <rongxing.sun@gmail.com>
2012-03-20 14:26:33 -0700

    first draft of term extraction

---
+++ nlp/wiki_disambiguator.py
@@ -0,0 +1,23 @@
+import urllib2
+import json 
+
+class WikiDisambiguator(object):
+    def disambiguate(self, text):
+        text = urllib2.quote(text)
+        url = "http://en.wikipedia.org/w/api.php?action=opensearch&search=%s&limit=10&namespace=0&format=json"
+        query_url = url % (text)
+        req = urllib2.Request(query_url)
+        r = urllib2.urlopen(req)
+        json_response = json.loads(r.read())
+        potential = json_response[1]
+        if potential:
+            return potential[0]
+        else:
+            return None
+
+def test():
+    diser = WikiDisambiguator()
+    print diser.disambiguate('fasdfsdf')
+
+if __name__ == '__main__':
+    test()
